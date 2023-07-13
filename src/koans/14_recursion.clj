eca57b56594523ffb5d1b06b4df2343fc2af9c92
Kiri <60819566+KiriCode1@users.noreply.github.com>
2023-07-13 22:55:56 +0300

    14_recursion complete

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,29 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
-  (loop [n   n
+  (loop [n n
          acc true]
     (if (= n 0)
-      __
+      true
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+         acc ()]
+    (if (seq coll)
+      (recur (rest coll) (conj acc (first coll)))
+      acc)))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         accum 1]
+    (if (zero? n)
+      accum
+      (recur (dec n) (* accum n)))))
 
 (meditations
   "Recursion ends with a base case"
@@ -27,7 +35,7 @@
   (= false (is-even? 1))
 
   "Having too many stack frames requires explicit tail calls with recur"
-  (= false (is-even-bigint? 100003N))
+  (= true (is-even-bigint? 100003N))
 
   "Reversing directions is easy when you have not gone far"
   (= '(1) (recursive-reverse [1]))
